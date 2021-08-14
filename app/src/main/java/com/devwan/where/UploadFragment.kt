a03b73be5c0dd5a5c4e962abfabd8994afcd6df8
5england <dongwan999@naver.com>
2021-08-14 23:29:55 +0900

    init

--- app/src/main/java/com/devwan/where/UploadFragment.kt
+++ app/src/main/java/com/devwan/where/UploadFragment.kt
@@ -1,59 +1,95 @@
 package com.devwan.where
 
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.CalendarContract.Attendees.query
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.fragment_upload.*
+import kotlinx.android.synthetic.main.fragment_upload.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UploadFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UploadFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var mContext: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
+
+    lateinit var filePath : String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload, container, false)
+        var rootView : View = inflater.inflate(R.layout.fragment_upload, container, false)
+
+        val btnSetImage = rootView.findViewById<ImageView>(R.id.btn_set_image)
+        btnSetImage.setOnClickListener(View.OnClickListener {
+            getPicture()
+        })
+
+        return rootView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UploadFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UploadFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun getPicture(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        intent.type = "image/*"
+        startActivityForResult(intent, 1000)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1000){
+            val uri : Uri = data!!.data!!
+            btn_set_image.setImageURI(uri)
+            btn_set_image.setPadding(0, 0, 0, 0)
+            btn_set_image.scaleType = ImageView.ScaleType.CENTER_CROP
+            val filePath = getImageFilePath(uri)
+
+            Log.d("path", ""+ uri + ", " + filePath)
+        }
+    }
+
+    fun getImageFilePath(contentUri : Uri): String{
+        if(contentUri.path!!.startsWith("/storage")) return contentUri.path!!
+
+        var columnIndex = 0
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = mContext.contentResolver.query(contentUri, projection, null, null, null)
+        if(cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        return cursor.getString(columnIndex)
+    }
+
+
 }
+
+
+
+
+
+
