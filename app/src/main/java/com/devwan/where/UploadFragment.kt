d3f15ca524487ffec59b25a9d854111769a6368a
5england <dongwan999@naver.com>
2021-08-17 18:46:07 +0900

    post image -> download image url

--- app/src/main/java/com/devwan/where/UploadFragment.kt
+++ app/src/main/java/com/devwan/where/UploadFragment.kt
@@ -21,6 +21,8 @@ import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
 import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.fragment_upload.*
 import kotlinx.android.synthetic.main.fragment_upload.view.*
 import java.io.File
@@ -29,6 +31,7 @@ import java.io.File
 class UploadFragment : Fragment() {
 
     lateinit var mContext: Context
+    var storage = Firebase.storage
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -39,8 +42,10 @@ class UploadFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    lateinit var filePath : String
-    lateinit var file : File
+//    lateinit var filePath : String
+//    lateinit var file : Uri
+
+    lateinit var imageUri : Uri
     lateinit var image : String
     lateinit var title : String
     lateinit var detail : String
@@ -58,19 +63,45 @@ class UploadFragment : Fragment() {
             getPicture()
         })
 
-        btn_upload.setOnClickListener{
-            if(file == null) Toast.makeText(context, "no Image", Toast.LENGTH_LONG).show()
+        rootView.btn_upload.setOnClickListener{
+            if(imageUri == null) Toast.makeText(context, "no Image", Toast.LENGTH_LONG).show()
             else if(address == null) Toast.makeText(context, "no address", Toast.LENGTH_LONG).show()
             else if(edit_title.text.toString().isBlank()) Toast.makeText(context, "no title", Toast.LENGTH_LONG).show()
             else uploadWhere()
+
+            uploadWhere()
         }
+
         return rootView
     }
-
 
     fun uploadWhere(){
         title = edit_title.text.toString()
         detail = edit_detail.text.toString()
+        uploadImageStorage()
+    }
+
+    fun uploadImageStorage(){
+        var storageRef = storage.reference
+        val ref = storageRef.child("images/" + imageUri.toString())
+        var uploadTask = ref.putFile(imageUri)
+
+        val urlTask = uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                Toast.makeText(mContext, "이미지가 정상적으로 업로드 되지 않았습니다.", Toast.LENGTH_LONG)
+                task.exception?.let {
+                    throw it
+                }
+            }
+            ref.downloadUrl
+        }.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                image = task.result.toString()
+            } else {
+                // Handle failures
+                // ...
+            }
+        }
     }
 
     fun getPicture(){
@@ -84,26 +115,26 @@ class UploadFragment : Fragment() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == 1000){
-            val uri : Uri = data!!.data!!
-            btn_set_image.setImageURI(uri)
+            imageUri = data!!.data!!
+            btn_set_image.setImageURI(imageUri)
             btn_set_image.setPadding(0, 0, 0, 0)
             btn_set_image.scaleType = ImageView.ScaleType.CENTER_CROP
-            filePath = getImageFilePath(uri)
-            file = File(filePath)
+//            filePath = getImageFilePath(curUri)
+//            file = Uri.fromFile(File(filePath))
         }
     }
 
-    fun getImageFilePath(contentUri : Uri): String{
-        if(contentUri.path!!.startsWith("/storage")) return contentUri.path!!
-
-        var columnIndex = 0
-        val projection = arrayOf(MediaStore.Images.Media.DATA)
-        val cursor = mContext.contentResolver.query(contentUri, projection, null, null, null)
-        if(cursor!!.moveToFirst()){
-            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        }
-        return cursor.getString(columnIndex)
-    }
+//    fun getImageFilePath(contentUri : Uri): String{
+//        if(contentUri.path!!.startsWith("/storage")) return contentUri.path!!
+//
+//        var columnIndex = 0
+//        val projection = arrayOf(MediaStore.Images.Media.DATA)
+//        val cursor = mContext.contentResolver.query(contentUri, projection, null, null, null)
+//        if(cursor!!.moveToFirst()){
+//            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+//        }
+//        return cursor.getString(columnIndex)
+//    }
 }
 
 
